9735111be3b6986c57f258ed89fc7b174c8fb063
Javi <itsjavii1610@gmail.com>
2026-01-14 09:12:16 +0100

    Estructura

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduzca su usuario y contraseña");
+
+        String usuario = sc.next();
+        String password =  sc.next();
+
+        String [] usuarios = {"username1", "rrh1", "admin1", "locker3"};
+        String [] passwords = {"user1234", "nvme2.0", "ddrtx4080", "qr650"};
+
+        for (int i = 0; i < usuarios.length; i++) {
+            
+        }
+
+        for (int i = 0; i< passwords.length; i++) {
+
+        }
+    }
 }
